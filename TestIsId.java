32582f85ddec4c5160fdfda451e7f715f3b48696
riawu <YuTing@r-207-103-25-172.comp.nus.edu.sg>
2015-08-19 13:21:42 +0800

    Second edit

--- TestIsId.java
+++ TestIsId.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class TestIsId
 {
     // Trying to edit something
+    // Second 
   public static void main( String [ ] args )
   {
     Scanner scan = new Scanner(System.in);
