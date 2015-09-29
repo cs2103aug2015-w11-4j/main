b3858f115d57e8670e32827e47615ea617cc2697
Foleosy <mywong@outlook.sg>
2015-09-29 13:18:23 +0800

    Implement Serializable on Task & Storage, Abstract-ed Storage, many changes to Storage
    Fixed save/read to/from file functions
    Added function to check for null entries and clear them
    Implemented comparator and fixed sort function
    Added main test case

--- F2DO/src/storage/Storage.java
+++ F2DO/src/storage/Storage.java
@@ -2,28 +2,28 @@ package storage;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.List;
 
 import objects.Task;
 
-public class Storage implements Comparator<Task> {
+public abstract class Storage implements Serializable, Comparator<Task> {
 	
     private static final String DEFAULT_DIRECTORY = "F2DO";
 	private static final String FILENAME = "F2DO.txt";
 	private static final String SAVED_DIRECTORY = "%s\\F2DO";
 	private static final String CHANGE_DIRECTORY = "user.dir";
 	
-	private ArrayList<Task> taskList = new ArrayList<Task>();
+	private static ArrayList<Task> taskList = new ArrayList<Task>();
 	private String saveFolder;
 	
 	public Storage(String directory) {
@@ -60,28 +60,34 @@ public class Storage implements Comparator<Task> {
 	}
 	
 	// Takes in task object created by parser and adds it to ArrayList
-	public void addTask(Task newTask) {
+	public static void addTask(Task newTask) {
+		nullRemovalCheck();
 		taskList.add(newTask);
 		saveToFile();
 	}
 	
 	// Deletes the task object located at the index supplied by the user
-	public void deleteTask(int taskNumber) {
+	public static void deleteTask(int taskNumber) {
 		taskList.remove(taskNumber);
+		nullRemovalCheck();
+		
 		saveToFile();
 	}
 	
 	// Updates the desired task with the new information
-	public void updateTask(int taskNumber, String newTitle, 
-			               Date newStartDate, Date newEndDate, String newCategory) {
+	public static void updateTask(int taskNumber, String newTitle, 
+			               Date newStartDate, Date newEndDate) {
+		nullRemovalCheck();
 		taskList.get(taskNumber).setTaskName(newTitle);
 		taskList.get(taskNumber).setStartDate(newStartDate);
 		taskList.get(taskNumber).setEndDate(newEndDate);
-		taskList.get(taskNumber).setCategory(newCategory);
+//		taskList.get(taskNumber).setCategory(newCategory);
 		saveToFile();
 	}
 	
-	private void saveToFile() {
+	private static void saveToFile() {
+		nullRemovalCheck();
+		
 		try {
 			FileOutputStream fout = new FileOutputStream(FILENAME);
 			ObjectOutputStream oos = new ObjectOutputStream(fout);
@@ -93,7 +99,7 @@ public class Storage implements Comparator<Task> {
 		}
 	}
 	
-	public void readFromFile() {
+	public static void readFromFile() {
 		try {
 			FileInputStream fin = new FileInputStream(FILENAME);
 			ObjectInputStream ois = new ObjectInputStream(fin);
@@ -108,23 +114,87 @@ public class Storage implements Comparator<Task> {
 		}
 	}
 	
-	public void displayTaskList() {
-		for (Task t: taskList) {
-			System.out.print(t);
+	public static void displayTaskList() {
+		nullRemovalCheck();
+		
+		for (int i = 0; i < taskList.size(); i++) {
+			System.out.println("Task ID: " + taskList.get(i).getTaskID() + 
+					"\nTask Name: " + taskList.get(i).getTaskName() + 
+					"\nStart Date: " + taskList.get(i).getStartDate() + 
+					"\nEnd Date: " + taskList.get(i).getEndDate());
 		}
 	}
 
-	@Override
-	public int compare(Task t1, Task t2) {
-		if (t1.getEndDate() == null || t2.getEndDate() == null) {
-			return 0;
+	// This method ensures that no empty tasks (after removal/updating) remain
+	private static void nullRemovalCheck() {
+		for (int i = 0; i < taskList.size(); i++) {
+			if (taskList.get(i).getTaskName() == null) {
+				taskList.remove(i); 
+			}
 		}
-		
-		return t1.getEndDate().compareTo(t2.getEndDate());
+	}
+
+    public static Comparator<Task> taskComparator = new Comparator<Task>() {
+    
+    	@Override
+    	public int compare(Task t1, Task t2) {
+			if (t1.getEndDate() == null || t2.getEndDate() == null) {
+				return 0;
+			}
+			
+			return t1.getEndDate().compareTo(t2.getEndDate());
+		}
+    };
+   
+	public static void sortTaskList() {
+		Collections.sort(taskList, taskComparator);
+		saveToFile();
 	}
 	
-	public void sortTaskList() {
-		Collections.sort(taskList);
-		saveToFile();
+	public static void main(String[] args) {		
+		taskList.clear();
+		readFromFile();
+		
+		String dateInput1 = "24/12/2015";
+		Date date1 = null;
+		try {
+			date1 = new SimpleDateFormat("dd/MM/yyyy").parse(dateInput1);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		Task testTask1 = new Task();
+		taskList.add(testTask1);
+		taskList.get(0).setTaskID(1);
+		taskList.get(0).setStartDate(date1);
+		taskList.get(0).setEndDate(date1);
+		taskList.get(0).setTaskName("z testing Task 1");
+		
+		String dateInput2 = "23/12/2015";
+		Date date2 = null;
+		try {
+			date2 = new SimpleDateFormat("dd/MM/yyyy").parse(dateInput2);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		Task testTask2 = new Task();
+		taskList.add(testTask2);
+		taskList.get(1).setTaskID(2);
+		taskList.get(1).setStartDate(date2);
+		taskList.get(1).setEndDate(date2);
+		taskList.get(1).setTaskName("y testing Task 2");
+		
+		displayTaskList();
+		updateTask(0, "zzz testing Task 1", date1, date1);
+		System.out.println("\n\n");
+		displayTaskList();
+		sortTaskList();
+		System.out.println("\n\n");
+		displayTaskList();
+		deleteTask(1);
+		System.out.println("\n\n");
+		displayTaskList();
+
 	}
 }
