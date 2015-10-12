662965eeeb687aec0a887de5288e404b73dba28f
Foleosy <mywong@outlook.sg>
2015-10-12 08:37:08 +0800

    Added comparator for floating tasks, return boolean for saving/reading
    !Untested!
    Looking towards adding assert functions instead of boolean if possible

--- F2DO/src/storage/Storage.java
+++ F2DO/src/storage/Storage.java
@@ -35,9 +35,7 @@ public class Storage implements Serializable {
 	static {
 		saveFolder = DEFAULT_DIRECTORY;
 		createSaveDir(DEFAULT_DIRECTORY);
-		if (!(isEmptyFile())) {
-			readFromFile();
-		}
+		readFromFile();
 	}
 		
 	public Storage(String directory) {
@@ -48,6 +46,10 @@ public class Storage implements Serializable {
 	public Storage() {
 		saveFolder = DEFAULT_DIRECTORY;
 		createSaveDir(DEFAULT_DIRECTORY);
+	}
+	
+	public static ArrayList<Task> getTaskList() {
+		return taskList;
 	}
 	
 	private static void createSaveDir(String directory) {
@@ -74,78 +76,114 @@ public class Storage implements Serializable {
 	}
 	
 	// Takes in task object created by parser and adds it to ArrayList
-	public static void addTask(Task newTask) {
+	public static boolean addTask(Task newTask) {
 		nullRemovalCheck();
 		taskList.add(newTask);
-		saveToFile();
+		
+		if (saveToFile()) {
+			return true;
+		}
+		
+		return false;
 	}
 	
 	// Deletes the task object located at the index supplied by the user
-	public static void deleteTask(int taskNumber) {
+	public static boolean deleteTask(int taskNumber) {
 		taskList.remove(taskNumber);
 		nullRemovalCheck();
 		
-		saveToFile();
+		if (saveToFile()) {
+			return true;
+		}
+		
+		return false;
 	}
 	
 	// Updates the desired task with the new information
-	public static void updateTask(int taskNumber, String newTitle, 
+	public static boolean updateTask(int taskNumber, String newTitle, 
 			               Date newStartDate, Date newEndDate) {
 		nullRemovalCheck();
 		taskList.get(taskNumber).setTaskName(newTitle);
 		taskList.get(taskNumber).setStartDate(newStartDate);
 		taskList.get(taskNumber).setEndDate(newEndDate);
-		saveToFile();
+		
+		if (saveToFile()) {
+			return true;
+		}
+		
+		return false;
 	}
 	
-	public static void saveToFile() {
+	// Saves the ArrayList into an XML file.
+	public static boolean saveToFile() {
+		boolean isSaveSuccess = true;
 		nullRemovalCheck();
 		XMLEncoder encoder = null;
+		
 		try {
 			FileOutputStream fos = new FileOutputStream(FILENAME);
 			BufferedOutputStream bos = new BufferedOutputStream(fos);
 			encoder = new XMLEncoder(bos);
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			isSaveSuccess = false;
 		}
+		
 		encoder.writeObject(taskList);
 		encoder.close();
+		
+		return isSaveSuccess;
 	}
 	
+	// Loads the ArrayList with data from the XML file.
 	@SuppressWarnings("unchecked")
-	private static void readFromFile() {
+	private static boolean readFromFile() {
+
+		if (isEmptyFile()) {
+			return false;
+		}
+		
+		boolean isReadSuccess = true;
+		nullRemovalCheck();
 		XMLDecoder decoder = null;
+				
 		try {
 			FileInputStream fis = new FileInputStream(FILENAME);
 			BufferedInputStream bis = new BufferedInputStream(fis);
 			decoder = new XMLDecoder(bis);
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			isReadSuccess = false;
 		}
+		
 		taskList = (ArrayList<Task>)decoder.readObject();
+		
+		return isReadSuccess;
 	}
 	
+	// Checks if the file contains any data.
 	private static boolean isEmptyFile() {
 		FileReader fr = null;
+		boolean isNothingRead = true;
+		
 		try {
 			fr = new FileReader(FILENAME);
-		} catch (FileNotFoundException e2) {
-			e2.printStackTrace();
-		}
-		try {
-			if (fr.read()==-1){
-				return true;
-			}
-		} catch (IOException e) {
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		return false;
+		
+		try {
+			if (fr.read() == -1) {
+				return isNothingRead;
+			}
+		} catch (IOException e) {
+			isNothingRead = false;
+		}
+		
+		return isNothingRead;
 	}
 	
-	public static ArrayList<Task> getTaskList() {
-		return taskList;
-	}
 	
+	
+	// Testing driver to show existing saved tasks
 	public static void displayTaskList() {
 		nullRemovalCheck();
 		
@@ -156,39 +194,61 @@ public class Storage implements Serializable {
 					"\nEnd Date: " + taskList.get(i).getEndDate());
 		}
 	}
+	
+	// Organizes the tasks to optimize displaying
+	public static boolean sortTaskList() {
+		nullRemovalCheck();
+		Collections.sort(taskList, taskComparator);
 
-	// This method ensures that no empty tasks (after removal/updating) remain
+		if (saveToFile()) {
+			return true;
+		}
+	
+		return false;
+	}
+
+	// Overriding comparator to compare by start/end dates.
+	public static Comparator<Task> taskComparator = new Comparator<Task>() {
+	    
+        @Override
+		public int compare(Task t1, Task t2) {
+			
+        	if (t1.getEndDate() != null && t2.getEndDate() != null) {
+				return t1.getEndDate().compareTo(t2.getEndDate());
+			} else if ((t1.getStartDate() == null && t1.getEndDate() != null && t2.getStartDate() != null)
+						|| (t1.getEndDate() != null && t2.getEndDate() == null && t2.getStartDate() != null)) {
+				return t1.getEndDate().compareTo(t2.getStartDate());
+			} else if ((t1.getStartDate() != null && t2.getStartDate() == null && t2.getEndDate() != null)
+						|| (t1.getEndDate() == null && t1.getStartDate() != null && t2.getEndDate() != null)) {
+				return t1.getStartDate().compareTo(t2.getEndDate());
+			} else if (t1.getStartDate() != null && t2.getStartDate() != null) {
+				return t1.getStartDate().compareTo(t2.getStartDate());
+			}
+        	
+        	// If either t1 or t2 is a floating task, or both.
+        	return t1.getTaskName().compareTo(t2.getTaskName());
+		}
+	};
+	
+	// Ensures that no empty tasks are in the ArrayList at any point in time.
 	private static void nullRemovalCheck() {
 		
 		for (int i = 0; i < taskList.size(); i++) {
-			if (taskList.get(i).getTaskName() == null) {
+			if (taskList.get(i).getTaskName() == null || taskList.get(i).getTaskName().equals("")
+					|| taskList.get(i).getTaskName().isEmpty()) {
 				taskList.remove(i); 
+				i--;
 			}
 		}
 	}
-
-	public static Comparator<Task> taskComparator = new Comparator<Task>() {
-    
-        @Override
-		public int compare(Task t1, Task t2) {
-			if (t1.getEndDate() == null || t2.getEndDate() == null) {
-				return 0;
-			}
-			
-			return t1.getEndDate().compareTo(t2.getEndDate());
-		}
-	};
-   
-	public static void sortTaskList() {
-		Collections.sort(taskList, taskComparator);
-		saveToFile();
-	}
+	
 	
 	public static void main(String[] args) {		
 		saveFolder = DEFAULT_DIRECTORY;
 		createSaveDir(DEFAULT_DIRECTORY);
 		createSaveFile();
 		taskList.clear();
+		
 		if (!(isEmptyFile())) {
 			readFromFile();
 		}
@@ -213,57 +273,24 @@ public class Storage implements Serializable {
 		taskList.add(testTask2);
 		taskList.get(1).setTaskID(2);
 		try {
-		taskList.get(1).setStartDate(sdf.parse(dateInput2));
-		taskList.get(1).setEndDate(sdf.parse(dateInput2));
-		} catch (ParseException e1) {
-			e1.printStackTrace();
-		}
-		taskList.get(1).setTaskName("y testing Task 2");
-		
-		displayTaskList();
-//		updateTask(1, "yyy testing Task 2", date1, date1);
-		System.out.println("\n\n");
-		displayTaskList();
-		sortTaskList();
-		System.out.println("\n\n");
-		displayTaskList();
-//		deleteTask(1);
-		System.out.println("\n\n");
-		displayTaskList();
-
-	}
-}
-
-/*
- * Write and read task objects directly
- * 
-private static void saveToFile() {
-	nullRemovalCheck();
-	
-	try {
-		FileOutputStream fout = new FileOutputStream(FILENAME);
-		ObjectOutputStream oos = new ObjectOutputStream(fout);
-		oos.writeObject(taskList);
-		fout.flush();
-		fout.close();
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-}
-public static void readFromFile() {
-	try {
-		FileInputStream fin = new FileInputStream(FILENAME);
-		ObjectInputStream ois = new ObjectInputStream(fin);
-		try {
-			taskList = (ArrayList<Task>)ois.readObject();
-		} catch (StreamCorruptedException | EOFException | ClassNotFoundException e) {
+			taskList.get(1).setStartDate(sdf.parse(dateInput2));
+			taskList.get(1).setEndDate(sdf.parse(dateInput2));
+		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		fin.close();
-		ois.close();
 		
-	} catch (IOException e) {
-		e.printStackTrace();
+		taskList.get(1).setTaskName("y testing Task 2");
+		displayTaskList();
+
+		if (sortTaskList()) {
+			System.out.println("\n\n");
+			displayTaskList();
+		}
+	
+/*		if (deleteTask(1)) {
+			System.out.println("\n\n");
+			displayTaskList();
+		}
+*/
 	}
 }
-*/
