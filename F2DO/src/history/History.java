f78c2df0724160b62396b83832b6b717f402cfd5
Sufyan <yan91work@gmail.com>
2015-10-16 17:05:59 +0800

    Added skeleton History class

---
+++ F2DO/src/history/History.java
@@ -0,0 +1,14 @@
+package history;
+
+import java.util.Stack;
+
+public class History {
+	private static Stack<String> history = new Stack<String>();
+	
+	public boolean addToHistory(String input){
+		
+		
+		return false;
+	}
+	
+}
