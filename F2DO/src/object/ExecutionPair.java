2b126d8d8759c22e0e485fd99fb0d3dd78e7b3de
Yu Ting <a0118005@u.nus.edu>
2015-10-26 01:02:28 +0800

    Add comment for ExecutionPair.java

--- F2DO/src/object/ExecutionPair.java
+++ F2DO/src/object/ExecutionPair.java
@@ -5,24 +5,41 @@ import type.CommandType;
 public class ExecutionPair {
 	private CommandType _cmd = CommandType.INVALID;
 	private String _content = null;
-	private Task _oldTask = null;
-	private Task _newTask = null;
+	private Task _befExeTask = null;
+	private Task _aftExeTask = null;
 	
-	public ExecutionPair(CommandType cmd, Task oldTask, Task newTask) {
+	/**
+	 * Constructor for editable command type.
+	 * For example, ADD, DELETE, EDIT, DONE and UNDONE.
+	 * @param cmd - editable command type
+	 * @param befExeTask - task before execution
+	 * @param aftExeTask - task after execution
+	 */
+	public ExecutionPair(CommandType cmd, Task befExeTask, Task aftExeTask) {
 		_cmd = cmd;
-		_oldTask = oldTask;
-		_newTask = newTask;
+		_befExeTask = befExeTask;
+		_aftExeTask = aftExeTask;
 	}
 	
+	/**
+	 * Constructor for non-editable command type.
+	 * For example, SEARCH, SHOW, HELP AND HOME.
+	 * @param cmd - non-editable command type
+	 * @param content - content of the parsing result
+	 */
 	public ExecutionPair(CommandType cmd, String content) {
 		_cmd = cmd;
 		_content = content;
 	}
 	
+	/**
+	 * Get undo execution result.
+	 * @return undo result
+	 */
 	public Result getUndo() {
 		CommandType newCmd = getReverseCmd();
 		if (isEditableType(newCmd)) {
-			return new Result(newCmd, _oldTask);
+			return new Result(newCmd, _befExeTask);
 		} else {
 			Result result = new Result();
 			result.setCommand(newCmd);
@@ -31,9 +48,13 @@ public class ExecutionPair {
 		}
 	}
 	
+	/**
+	 * Get redo execution result.
+	 * @return redo result
+	 */
 	public Result getRedo() {
 		if (isEditableType(_cmd)) {
-			return new Result(_cmd, _newTask);
+			return new Result(_cmd, _aftExeTask);
 		} else {
 			Result result = new Result();
 			result.setCommand(_cmd);
@@ -42,6 +63,11 @@ public class ExecutionPair {
 		}
 	}
 	
+	/**
+	 * Determine if the command type is editable.
+	 * @param cmd - command type
+	 * @return true if command is editable type; false otherwise
+	 */
 	private boolean isEditableType(CommandType cmd) {
 		return cmd == CommandType.ADD ||
 				cmd == CommandType.DELETE ||
@@ -50,6 +76,11 @@ public class ExecutionPair {
 				cmd == CommandType.UNDONE;
 	}
 	
+	/**
+	 * Get the reverse of the command type.
+	 * For example, ADD <-> DELETE, DONE <-> UNDONE
+	 * @return reverse command type
+	 */
 	private CommandType getReverseCmd() {
 		switch (_cmd) {
 			case ADD:
