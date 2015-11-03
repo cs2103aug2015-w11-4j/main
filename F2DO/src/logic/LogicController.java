dd7f6c46288d7e977fe97cad602c90af348015e1
Foleosy <mywong@outlook.sg>
2015-11-03 21:28:08 +0800

    Added logic tests
    Did not manage to hit 100% for comparator with 2^4 (all possible
    combination) number of cases.

--- F2DO/src/logic/LogicController.java
+++ F2DO/src/logic/LogicController.java
@@ -297,7 +297,7 @@ public class LogicController {
 	};
 	
 	// For testing purpose
-	public static void main(String[] args) {
+/*	public static void main(String[] args) {
 		System.out.print("Command: ");
 		Scanner scanner = new Scanner(System.in);
 		String input = scanner.nextLine();
@@ -310,5 +310,5 @@ public class LogicController {
 		for (int i = 0; i < displayList.size(); i++) {
 			System.out.println((i+1) + ". " + displayList.get(i).getTaskName());
 		}
-	}
+	}*/
 }
