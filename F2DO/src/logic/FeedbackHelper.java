559b94cb52b7ea821f9f276fd1e801863113f070
Foleosy <mywong@outlook.sg>
2015-11-03 23:42:59 +0800

    Correct typo in feedback helper.

--- F2DO/src/logic/FeedbackHelper.java
+++ F2DO/src/logic/FeedbackHelper.java
@@ -7,20 +7,20 @@ public class FeedbackHelper {
 	public static final String MSG_DELETE = "Feedback: %1$s has been deleted!";
 	public static final String MSG_SEARCH = "Feedback: Return %1$s result%2$s!";
 	public static final String MSG_SHOW_ALL = "Feedback: Show all task%1$s!";
-	public static final String MSG_SHOW_DONE = "Feedback: Show %1$s complete task%2$s!";
+	public static final String MSG_SHOW_DONE = "Feedback: Show %1$s completed task%2$s!";
 	public static final String MSG_SHOW_UNDONE = "Feedback: Show %1$s incomplete task%2$s!";
 	public static final String MSG_DONE = "Feedback: %1$s has been marked as completed!";
-	public static final String MSG_UNDONE = "Feedback: %1$s has been marked as imcomplete!";
+	public static final String MSG_UNDONE = "Feedback: %1$s has been marked as incomplete!";
 	public static final String MSG_HELP = "Feedback: Show HELP!";
 	public static final String MSG_HOME = "Feedback: Show home page!";
 	
 	public static final String ERROR_ADD = "Feedback: %1$s cannot be added!";
 	public static final String ERROR_CAT = "Feedback: Category %1$s cannot be added!";
 	public static final String ERROR_NO_INDEX = "Feedback: The entered number does not exist!";
-	public static final String ERROR_SEARCH = "Feedback: No result found!";
+	public static final String ERROR_SEARCH = "Feedback: No results found!";
 	public static final String ERROR_EDIT = "Feedback: %1$s cannot be modified!";
 	public static final String ERROR_SHOW_NO_ALL = "Feedback: No task!";
-	public static final String ERROR_SHOW_NO_DONE = "Feedback: No complete task!";
+	public static final String ERROR_SHOW_NO_DONE = "Feedback: No completed task!";
 	public static final String ERROR_SHOW_NO_UNDONE = "Feedback: No incomplete task!";
 	public static final String ERROR_NO_ACTION = "Feedback: No action is done!";
 	public static final String ERROR_INVALID_COMMAND = "Feedback: The command entered does not exist!";
