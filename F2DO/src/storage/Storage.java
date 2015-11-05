e0490ad60dc113cbeb10f239e416c923dcb28501
Foleosy <mywong@outlook.sg>
2015-11-06 06:47:04 +0800

    Updated storage

--- F2DO/src/storage/Storage.java
+++ F2DO/src/storage/Storage.java
@@ -1,247 +1,194 @@
+//@@author Ming Yang
 package storage;
 
-import java.beans.XMLDecoder;
-import java.io.EOFException;
+import object.Task;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentSkipListMap;
 
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
-
-import objects.Task;
-
-public abstract class Storage implements Serializable, Comparator<Task> {
-	
+/**
+ * Storage class maintains file input and output related matters.
+ */
+public class Storage {
 	private static final String DEFAULT_DIRECTORY = "F2DO";
-	private static final String FILENAME = "F2DO.txt";
-	private static final String SAVED_DIRECTORY = "%s\\F2DO";
+	private static final String FILENAME = "F2DO.json";
+	private static final String SAVED_DIRECTORY = "%s/F2DO";
 	private static final String CHANGE_DIRECTORY = "user.dir";
+	private static final String DEFAULT_FILE_PATH = DEFAULT_DIRECTORY + "/" + FILENAME; 
+	private static final String PROPERTIES_FILE_PATH = DEFAULT_DIRECTORY + "/config.properties";
+	private static final String PROPERTIES_SAVE_FILE = "filepath";
 	
-	private static ArrayList<Task> taskList = new ArrayList<Task>();
-	private static String saveFolder;
+	private static File _saveFolder = new File(DEFAULT_DIRECTORY);
+	private static File _saveFile =  new File(DEFAULT_FILE_PATH);
+	private static File _propertiesFile = new File(PROPERTIES_FILE_PATH);
 	
-	// Initialize storage class
+	private static Properties properties = new Properties();
+	
+	/**
+	 * Initialize storage class.
+	 */
 	static {
-		saveFolder = DEFAULT_DIRECTORY;
-		createSaveDir(DEFAULT_DIRECTORY);
-		readFromFile();
+		if(createPropertiesFile()) {
+			readPropertiesFile();
+		} 
 	}
 	
-	public Storage(String directory) {
-		saveFolder = String.format(SAVED_DIRECTORY, directory);
-		createSaveDir(saveFolder);
-	}
-	
-	public Storage() {
-		saveFolder = DEFAULT_DIRECTORY;
-		createSaveDir(DEFAULT_DIRECTORY);
-	}
-	
-	private static void createSaveDir(String directory) {
-		File dir = new File(directory);
+	/**
+	 * Set new folder.
+	 * @param newFolderPath - path of new folder
+	 * @return true if the new folder is set successfully; false otherwise
+	 */
+	public static boolean setFolder(String newFolderPath) {
+		File prevFolder = _saveFolder;
+		File prevFile = _saveFile;
+		boolean isSuccessful = false;
+		ConcurrentSkipListMap<Integer, Task> taskList = readTasks();
 		
-		if (!dir.exists()) {
-			dir.mkdir();
-		}
-		
-		System.setProperty(CHANGE_DIRECTORY, directory);
-		createSaveFile();
-	}
-	
-	private static void createSaveFile() {
-		File file = new File(FILENAME);
-		
-		if (!file.exists()) {
-			try {
-				file.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	// Takes in task object created by parser and adds it to ArrayList
-	public static void addTask(Task newTask) {
-		nullRemovalCheck();
-		taskList.add(newTask);
-		saveToFile();
-	}
-	
-	// Deletes the task object located at the index supplied by the user
-	public static void deleteTask(int taskNumber) {
-		taskList.remove(taskNumber);
-		nullRemovalCheck();
-		
-		saveToFile();
-	}
-	
-	// Updates the desired task with the new information
-	public static void updateTask(int taskNumber, String newTitle, 
-			               Date newStartDate, Date newEndDate) {
-		nullRemovalCheck();
-		taskList.get(taskNumber).setTaskName(newTitle);
-		taskList.get(taskNumber).setStartDate(newStartDate);
-		taskList.get(taskNumber).setEndDate(newEndDate);
-//		taskList.get(taskNumber).setCategory(newCategory);
-		saveToFile();
-	}
-	
-	public static void saveToFile() {
-		nullRemovalCheck();
-		
-<<<<<<< HEAD
-		for (int i = 0; i < taskList.size(); i++) {
-			GregorianCalendar gregCal = new GregorianCalendar();
-			gregCal.setTime(taskList.get(i).getStartDate());
-			gregCal.setTime(taskList.get(i).getEndDate());
-			try {
-				XMLGregorianCalendar xmlDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(gregCal);
-			} catch (DatatypeConfigurationException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		XMLEncoder encoder = null;
-=======
->>>>>>> origin/master
-		try {
-			FileOutputStream fout = new FileOutputStream(FILENAME);
-			ObjectOutputStream oos = new ObjectOutputStream(fout);
-			oos.writeObject(taskList);
-			fout.flush();
-			fout.close();
-		} catch (IOException e) {
+		try{
+			String folderPath = String.format(SAVED_DIRECTORY, newFolderPath);
+			String filePath = folderPath + "/" + FILENAME;
+			_saveFolder = new File(folderPath);
+			_saveFile = new File(filePath);
+
+			if (createFolder() && createFile()) {
+				isSuccessful = writePropertiesFile(filePath);
+				if ((prevFile != null) && (prevFile.exists()) ) {
+					writeTasks(taskList);		// Copy the task list into new file
+					prevFile.delete();
+				}
+			} 
+		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			if (!isSuccessful) {
+				_saveFolder = prevFolder;
+				_saveFile = prevFile;
+			}
 		}
+		return isSuccessful;
 	}
 	
-	@SuppressWarnings("unchecked")
-	private static void readFromFile() {
-		XMLDecoder decoder = null;
+	/**
+	 * Create properties file.
+	 * @return true if properties file is created successfully; false otherwise
+	 */
+	private static boolean createPropertiesFile() {
 		try {
-			FileInputStream fin = new FileInputStream(FILENAME);
-			ObjectInputStream ois = new ObjectInputStream(fin);
-			try {
-				taskList = (ArrayList<Task>)ois.readObject();
-			} catch (EOFException | ClassNotFoundException e) {
-				e.printStackTrace();
+			if (!_propertiesFile.exists()) {
+				_saveFolder = new File(DEFAULT_DIRECTORY);
+				_saveFile =  new File(DEFAULT_FILE_PATH);
+				
+				if (createFolder()) {
+					createFile();
+				}
+				
+				FileOutputStream fos = new FileOutputStream(PROPERTIES_FILE_PATH);
+				
+				properties.setProperty(PROPERTIES_SAVE_FILE, DEFAULT_FILE_PATH);
+				properties.store(fos, "Properties file of F2DO");
+				fos.close();
 			}
-			ois.close();
-			fin.close();
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Read the stored file path from properties file.
+	 * @return true if the file path is read successfully; false otherwise
+	 */
+	private static boolean readPropertiesFile() {
+		try {
+			FileInputStream fis = new FileInputStream(PROPERTIES_FILE_PATH);
+			properties.load(fis);
 			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static ArrayList<Task> getTaskList() {
-		return taskList;
-	}
-	
-	public static void displayTaskList() {
-		nullRemovalCheck();
-		
-		for (int i = 0; i < taskList.size(); i++) {
-			System.out.println("Task ID: " + taskList.get(i).getTaskID() + 
-					"\nTask Name: " + taskList.get(i).getTaskName() + 
-					"\nStart Date: " + taskList.get(i).getStartDate() + 
-					"\nEnd Date: " + taskList.get(i).getEndDate());
-		}
-	}
-
-	// This method ensures that no empty tasks (after removal/updating) remain
-	private static void nullRemovalCheck() {
-		for (int i = 0; i < taskList.size(); i++) {
-			if (taskList.get(i).getTaskName() == null) {
-				taskList.remove(i); 
-			}
-		}
-	}
-
-	public static Comparator<Task> taskComparator = new Comparator<Task>() {
-    
-        @Override
-		public int compare(Task t1, Task t2) {
-			if (t1.getEndDate() == null || t2.getEndDate() == null) {
-				return 0;
-			} else if (t1.getEndDate().equals(t2.getEndDate())) {
-				return t1.getTaskName().compareTo(t2.getTaskName());
-			}
+			String filePath = properties.getProperty(PROPERTIES_SAVE_FILE);
+			_saveFile = new File(filePath);
+			fis.close();
 			
-			return t1.getEndDate().compareTo(t2.getEndDate());
+			if (!_saveFile.exists()) {
+				_propertiesFile.delete();
+				createPropertiesFile();
+			}
+		} catch (Exception e) {
+			return false;
 		}
-	};
-   
-	public static void sortTaskList() {
-		Collections.sort(taskList, taskComparator);
-		saveToFile();
+		return true;
 	}
 	
-	public static void main(String[] args) {		
-		createSaveFile();
-		taskList.clear();
-		readFromFile();
-		
-		String dateInput1 = "24/12/2015";
-		Date date1 = null;
+	/**
+	 * Write customized save folder to properties file.
+	 * @param path - customized path
+	 * @return true if the property is written successfully; false otherwise
+	 */
+	private static boolean writePropertiesFile(String path) {
 		try {
-			date1 = new SimpleDateFormat("dd/MM/yyyy").parse(dateInput1);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		
-		Task testTask1 = new Task();
-		taskList.add(testTask1);
-		taskList.get(0).setTaskID(1);
-		taskList.get(0).setStartDate(date1);
-		taskList.get(0).setEndDate(date1);
-		taskList.get(0).setTaskName("z testing Task 1");
-		
-		String dateInput2 = "23/12/2015";
-		Date date2 = null;
-		try {
-			date2 = new SimpleDateFormat("dd/MM/yyyy").parse(dateInput2);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+			FileOutputStream fos = new FileOutputStream(PROPERTIES_FILE_PATH);
 
-		Task testTask2 = new Task();
-		taskList.add(testTask2);
-		taskList.get(1).setTaskID(2);
-		taskList.get(1).setStartDate(date2);
-		taskList.get(1).setEndDate(date2);
-		taskList.get(1).setTaskName("y testing Task 2");
-		
-		displayTaskList();
-		/*
-		try {
-			updateTask(0, "zzz", sdf.parse(dateInput2), sdf.parse(dateInput2));
-		} catch (ParseException e) {
-			e.printStackTrace();
+			properties.setProperty(PROPERTIES_SAVE_FILE, path);
+			properties.store(fos, "Properties file of F2DO");
+			fos.close();
+		} catch (Exception e) {
+			return false;
 		}
-		*/
-		System.out.println("\n\n");
-		displayTaskList();
-		sortTaskList();
-		System.out.println("\n\n");
-		displayTaskList();
-		deleteTask(1);
-		System.out.println("\n\n");
-		displayTaskList();
-
+		return true;
 	}
+
+	/**
+	 * Create folder if it does not exist.
+	 * @return true if the folder is created successfully; false otherwise
+	 */
+	private static boolean createFolder() {
+		try {
+			if (!_saveFolder.exists()) {
+				_saveFolder.mkdir();
+			}
+			System.setProperty(CHANGE_DIRECTORY, _saveFolder.getPath());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Create file if it does not exist.
+	 * @return true if the file is created successfully; false otherwise
+	 */
+	private static boolean createFile() {
+		try {
+			if (!_saveFile.exists()) {
+				StorageHelper.createJsonFile(_saveFile);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Read task list from the file.
+	 * @return task list
+	 */
+	public static ConcurrentSkipListMap<Integer, Task> readTasks() {
+		return StorageHelper.readJsonFile(_saveFile);
+	}
+	
+	/**
+	 * Write task list into the file.
+	 * @param taskList - task list to be written into the file
+	 * @return true if it is written into the file successfully; false otherwise
+	 */
+	public static boolean writeTasks(ConcurrentSkipListMap<Integer, Task> taskList) {
+		if (!_saveFile.exists()) {
+			_propertiesFile.delete();
+			createPropertiesFile();
+		}
+		return StorageHelper.writeJsonFile(_saveFile, taskList);
+	}
+	
 }
