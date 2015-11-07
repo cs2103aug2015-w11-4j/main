5c73c3e0d046bb077962a056ab744cbea4e9ae4d
Foleosy <mywong@outlook.sg>
2015-11-08 01:46:41 +0800

    Reconfigured DateTimeTest
    Used calendar to set date/timing

--- F2DO/tests/parser/DateTimeTest.java
+++ F2DO/tests/parser/DateTimeTest.java
@@ -4,6 +4,7 @@ package parser;
 import static org.junit.Assert.*;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -16,16 +17,36 @@ public class DateTimeTest {
 	private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
 	@Test
-	public void testSlashNumericDMY() {
+	public void testSlashNumericDMY() throws ParseException {
 		
 		logger.info("Starting SlashNumericDMY test");
 
-		assertEquals(getResult(2015, 12, 16, 12, 0), simpleFormat(DateTime.parse("16/12/2015")));
-		assertEquals(getResult(2015, 12, 17, 12, 0), simpleFormat(DateTime.parse("17/12/15")));
-		assertEquals(getResult(2015, 12, 6, 12, 0), simpleFormat(DateTime.parse("6/12/2015")));
-		assertEquals(getResult(2015, 12, 7, 12, 0), simpleFormat(DateTime.parse("07/12/2015")));
-		assertEquals(getResult(2015, 2, 16, 12, 0), simpleFormat(DateTime.parse("16/2/2015")));
-		assertEquals(getResult(2015, 2, 17, 12, 0), simpleFormat(DateTime.parse("17/02/2015")));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy HH:mm");
+		Calendar cal = Calendar.getInstance();
+		
+		cal.set(2015, 11, 16);
+		String testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16/12/2015")));
+		
+		cal.set(2015, 11, 17);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("17/12/15")));
+		
+		cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("6/12/2015")));
+		
+		cal.set(2015, 11, 7);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("07/12/2015")));
+		
+		cal.set(2015, 1, 16);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16/2/2015")));
+		
+		cal.set(2015, 1, 17);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("17/02/2015")));
 
 		logger.info("Successful end of SlashNumericDMY test");
 	}
@@ -35,12 +56,32 @@ public class DateTimeTest {
 	    
 	    logger.info("Starting HyphenNumericDMY test");
 
-		assertEquals(getResult(2015, 12, 16, 12, 0), simpleFormat(DateTime.parse("16-12-2015")));
-		assertEquals(getResult(2015, 12, 16, 12, 0), simpleFormat(DateTime.parse("16-12-15")));
-		assertEquals(getResult(2015, 12, 6, 12, 0), simpleFormat(DateTime.parse("6-12-2015")));
-		assertEquals(getResult(2015, 12, 6, 12, 0), simpleFormat(DateTime.parse("06-12-2015")));
-		assertEquals(getResult(2015, 2, 16, 12, 0), simpleFormat(DateTime.parse("16-2-2015")));
-		assertEquals(getResult(2015, 2, 16, 12, 0), simpleFormat(DateTime.parse("16-02-2015")));
+	    SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy HH:mm");
+		Calendar cal = Calendar.getInstance();
+		
+		cal.set(2015, 11, 16);
+		String testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16-12-2015")));
+
+		cal.set(2015, 11, 16);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16-12-15")));
+
+		cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("6-12-2015")));
+
+		cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("06-12-2015")));
+
+		cal.set(2015, 1, 16);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16-2-2015")));
+
+		cal.set(2015, 1, 16);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16-02-2015")));
 
 	    logger.info("Successful end of HyphenNumericDMY test");
 	}
@@ -50,11 +91,28 @@ public class DateTimeTest {
 	    
 		logger.info("Starting ShortDMY test");
 
-		assertEquals(getResult(2015, 12, 16, 12, 0), simpleFormat(DateTime.parse("16 Dec 2015")));
-		assertEquals(getResult(2015, 12, 16, 12, 0), simpleFormat(DateTime.parse("16 Dec 15")));
-		assertEquals(getResult(2015, 12, 6, 12, 0), simpleFormat(DateTime.parse("6 Dec 2015")));
-		assertEquals(getResult(2015, 12, 6, 12, 0), simpleFormat(DateTime.parse("06 Dec 2015")));
-		assertEquals(getResult(2015, 2, 8, 12, 0), simpleFormat(DateTime.parse("08 Feb 15")));
+	    SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy HH:mm");
+		Calendar cal = Calendar.getInstance();
+		
+		cal.set(2015, 11, 16);
+		String testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16 Dec 2015")));
+
+		cal.set(2015, 11, 16);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16 Dec 15")));
+
+		cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("6 Dec 2015")));
+
+		cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("06 Dec 2015")));
+
+		cal.set(2015, 1, 8);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("08 Feb 15")));
 
 	    logger.info("Successful end of ShortDMY test");
 	}
@@ -64,11 +122,28 @@ public class DateTimeTest {
 	    
 		logger.info("Starting SlashNumbericDM test");
 
-	    assertEquals(getResult(0, 12, 16, 12, 0), simpleFormat(DateTime.parse("16/12")));
-		assertEquals(getResult(0, 12, 6, 12, 0), simpleFormat(DateTime.parse("6/12")));
-		assertEquals(getResult(0, 12, 6, 12, 0), simpleFormat(DateTime.parse("06/12")));
-		assertEquals(getResult(0, 2, 16, 12, 0), simpleFormat(DateTime.parse("16/2")));
-		assertEquals(getResult(0, 2, 16, 12, 0), simpleFormat(DateTime.parse("16/02")));
+	    SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy HH:mm");
+		Calendar cal = Calendar.getInstance();
+		
+		cal.set(2015, 11, 16);
+		String testDate = sdf.format(cal.getTime());
+	    assertEquals(testDate, simpleFormat(DateTime.parse("16/12")));
+	    
+	    cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("6/12")));
+		
+		cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("06/12")));
+		
+		cal.set(2015, 1, 16);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16/2")));
+		
+		cal.set(2015, 1, 16);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16/02")));
 	
 	    logger.info("Successful end of SlashNumbericDM test");
 	}
@@ -77,12 +152,29 @@ public class DateTimeTest {
 	public void testHyphenNumbericDM() {
 	    
 		logger.info("Starting HyphenNumbericDM test");
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy HH:mm");
+		Calendar cal = Calendar.getInstance();
+		
+		cal.set(2015, 11, 16);
+		String testDate = sdf.format(cal.getTime());
+	    assertEquals(testDate, simpleFormat(DateTime.parse("16-12")));
 
-	    assertEquals(getResult(0, 12, 16, 12, 0), simpleFormat(DateTime.parse("16-12")));
-		assertEquals(getResult(0, 12, 6, 12, 0), simpleFormat(DateTime.parse("6-12")));
-		assertEquals(getResult(0, 12, 6, 12, 0), simpleFormat(DateTime.parse("06-12")));
-		assertEquals(getResult(0, 2, 16, 12, 0), simpleFormat(DateTime.parse("16-2")));
-		assertEquals(getResult(0, 2, 16, 12, 0), simpleFormat(DateTime.parse("16-02")));
+		cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("6-12")));
+
+		cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("06-12")));
+
+		cal.set(2015, 1, 16);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16-2")));
+
+		cal.set(2015, 1, 16);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16-02")));
 
 		logger.info("Successful end of HyphenNumbericDM test");
 	}
@@ -92,10 +184,24 @@ public class DateTimeTest {
 	    
 		logger.info("Starting ShortDM test");
 
-	    assertEquals(getResult(0, 12, 16, 12, 0), simpleFormat(DateTime.parse("16 Dec")));
-		assertEquals(getResult(0, 12, 6, 12, 0), simpleFormat(DateTime.parse("6 Dec")));
-		assertEquals(getResult(0, 12, 6, 12, 0), simpleFormat(DateTime.parse("06 Dec")));
-		assertEquals(getResult(0, 2, 16, 12, 0), simpleFormat(DateTime.parse("16 Feb")));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy HH:mm");
+		Calendar cal = Calendar.getInstance();
+		
+		cal.set(2015, 11, 16);
+		String testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16 Dec")));
+
+		cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("6 Dec")));
+
+		cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("06 Dec")));
+
+		cal.set(2015, 1, 16);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16 Feb")));
 		
 		logger.info("Successful end of ShortDM test");
 	}
@@ -104,11 +210,25 @@ public class DateTimeTest {
 	public void testShortMD() {
 	    
 		logger.info("Starting ShortMD test");
-
-	    assertEquals(getResult(0, 12, 16, 12, 0), simpleFormat(DateTime.parse("Dec 16")));
-		assertEquals(getResult(0, 12, 6, 12, 0), simpleFormat(DateTime.parse("Dec 6")));
-		assertEquals(getResult(0, 12, 6, 12, 0), simpleFormat(DateTime.parse("Dec 06")));
-		assertEquals(getResult(0, 2, 16, 12, 0), simpleFormat(DateTime.parse("Feb 16")));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy HH:mm");
+		Calendar cal = Calendar.getInstance();
+		
+		cal.set(2015, 11, 16);
+		String testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("Dec 16")));
+		
+		cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("Dec 6")));
+		
+		cal.set(2015, 11, 6);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("Dec 06")));
+		
+		cal.set(2015, 1, 16);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("Feb 16")));
 
 		logger.info("Successful end of ShortMD test");
 	}
@@ -130,14 +250,29 @@ public class DateTimeTest {
 	public void testMixture() {
 	    
 		logger.info("Starting Mixture test");
-
-	    assertEquals(getResult(0, 11, 4, 16, 0), simpleFormat(DateTime.parse("Nov 4 4pm")));
-		assertEquals(getResult(0, 2, 16, 7, 0), simpleFormat(DateTime.parse("16 Feb 7am")));
 		
-		// THESE THREE FAILED
-		assertEquals(getResult(0, 11, 4, 8, 30), simpleFormat(DateTime.parse("Nov 4 8.30")));
-		assertEquals(getResult(0, 2, 16, 22, 10), simpleFormat(DateTime.parse("16 Feb 22:10")));
-		assertEquals(getResult(2015, 2, 17, 22, 10), simpleFormat(DateTime.parse("17 Feb 2015 22:10")));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy HH:mm");
+		Calendar cal = Calendar.getInstance();
+		
+		cal.set(2015, 10, 4, 16, 0);
+		String testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("Nov 4 4pm")));
+		
+		cal.set(2015, 1, 16, 7, 0);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16 Feb 7am")));
+		
+		cal.set(2015, 10, 4, 8, 30);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("Nov 4 8.30")));
+		
+		cal.set(2015, 1, 16, 22, 10);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("16 Feb 22:10")));
+		
+		cal.set(2015, 1, 17, 22, 10);
+		testDate = sdf.format(cal.getTime());
+		assertEquals(testDate, simpleFormat(DateTime.parse("17 Feb 2015 22:10")));
 	
 	    logger.info("Successful end of Mixture test");
 	}
