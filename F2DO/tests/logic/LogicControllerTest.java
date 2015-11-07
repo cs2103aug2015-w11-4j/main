175e3caa910f585ead02d53d05a6121c87ba6422
Foleosy <mywong@outlook.sg>
2015-11-07 22:54:01 +0800

    Added more logiccontroller test cases

--- F2DO/tests/logic/LogicControllerTest.java
+++ F2DO/tests/logic/LogicControllerTest.java
@@ -70,9 +70,15 @@ public class LogicControllerTest {
 	    logger.info("Adding task with by works");
 
 	    //Test Edit
-	    assertNotEquals("Feedback: Homework has been edited!",
+	    assertEquals("Feedback: Homework has been modified!",
 	    			(LogicController.process("edit 1 Homework by Sunday", LogicController.getDisplayList())));
-	   	    
+	    logger.info("Editing task works.");
+
+	    //Test undone display
+	    assertEquals("Feedback: Show 1 incomplete task!",
+				(LogicController.process("show undone", LogicController.getDisplayList())));
+	    logger.info("Showing incomplete tasks works");
+
 	    //Testing completion marking
 		assertEquals("Feedback: Homework has been marked as completed!",
 					(LogicController.process("done 1", LogicController.getDisplayList())));
@@ -83,27 +89,90 @@ public class LogicControllerTest {
 					(LogicController.process("show done", LogicController.getDisplayList())));
 	    logger.info("Showing completed tasks works");
 	    
+	    //Testing marking done task done
+	    assertEquals("Feedback: Homework has been marked as completed!",
+					(LogicController.process("done 1", LogicController.getDisplayList())));
+		assertEquals("Feedback: Show 1 completed task!",
+					(LogicController.process("show done", LogicController.getDisplayList())));
+		logger.info("Marking completed task completed works");
+    
+	    //Testing marking done task undone
+		assertEquals("Feedback: Homework has been marked as incomplete!",
+					(LogicController.process("undone 1", LogicController.getDisplayList())));
+		logger.info("Marking done task as undone works.");
+		
+		//Testing marking undone task undone
+		assertEquals("Feedback: Homework has been marked as incomplete!",
+					(LogicController.process("undone 1", LogicController.getDisplayList())));
+		logger.info("Marking undone task as undone works.");
+	    
+		//Testing show all display
+		assertEquals("Feedback: Show all task!",
+					(LogicController.process("show all", LogicController.getDisplayList())));
+		logger.info("Showing all tasks works for 1 task");
+		
+		//Testing show all/done/undone for tasklists with more than one task
+		LogicController.process("add Meeting", LogicController.getDisplayList());
+		assertEquals("Feedback: Show all tasks!",
+					(LogicController.process("show all", LogicController.getDisplayList())));
+		assertEquals("Feedback: Show 2 incomplete tasks!",
+					(LogicController.process("show undone", LogicController.getDisplayList())));
+		LogicController.process("done 2", LogicController.getDisplayList());
+		LogicController.process("done 1", LogicController.getDisplayList());
+		assertEquals("Feedback: Show 2 completed tasks!",
+					(LogicController.process("show done", LogicController.getDisplayList())));
+		LogicController.process("delete 2", LogicController.getDisplayList());
+		logger.info("Showing all/done/undone tasks works for 2 or more tasks");
+
+		//Testing edit task with no modifications
+		LogicController.process("show done", LogicController.getDisplayList());
+	    assertEquals("Feedback: Nothing to be modified!",
+					(LogicController.process("edit 1", LogicController.getDisplayList())));
+	    logger.info("Editing task with no modification works.");
+		
+	    //Testing edit task with no modifications
+		LogicController.process("show done", LogicController.getDisplayList());
+	    assertEquals("Feedback: Nothing to be modified!",
+					(LogicController.process("edit 1", LogicController.getDisplayList())));
+	    logger.info("Editing task with no modification works.");
+	    
 	    //Clearing list for next test
+		LogicController.process("show done", LogicController.getDisplayList());
 		assertEquals("Feedback: Homework has been deleted!",
 					(LogicController.process("delete 1", LogicController.getDisplayList())));
 	    logger.info("Deleting task works.");
-
-	    //Testing incomplete task listing
+	    
+	    //Testing incomplete/complete/all task listing for blank tasklist
 		assertEquals("Feedback: No incomplete task!",
 					(LogicController.process("show undone", LogicController.getDisplayList())));
-	    logger.info("Showing undone works (if there's no task)");
+		assertEquals("Feedback: No completed task!",
+					(LogicController.process("show done", LogicController.getDisplayList())));
+		assertEquals("Feedback: No task!",
+					(LogicController.process("show all", LogicController.getDisplayList())));
+	    logger.info("Showing functions work (if there's no task)");
 	    
 	    //Testing out of bound deletion index
 	    assertEquals("Feedback: The entered number does not exist!",
-    			(LogicController.process("delete 0", LogicController.getDisplayList())));
+					(LogicController.process("delete 0", LogicController.getDisplayList())));
 	    assertEquals("Feedback: The entered number does not exist!",
-    			(LogicController.process("delete 1", LogicController.getDisplayList())));
+					(LogicController.process("delete 1", LogicController.getDisplayList())));
 	    assertEquals("Feedback: The entered number does not exist!",
-    			(LogicController.process("delete 2", LogicController.getDisplayList())));
+					(LogicController.process("delete 2", LogicController.getDisplayList())));
 	    logger.info("Deleting task numbers not acceptable throws the correct error feedback");
 	    
+	    //Out of bound testing for done/undone
+	    assertEquals("Feedback: The entered number does not exist!",
+					(LogicController.process("done 0", LogicController.getDisplayList())));
+	    assertEquals("Feedback: The entered number does not exist!",
+					(LogicController.process("undone 0", LogicController.getDisplayList())));
+	    
+	    //Testing out of bound for edit
+	    assertEquals("Feedback: The entered number does not exist!",
+					(LogicController.process("edit 0", LogicController.getDisplayList())));
+	    	    
 	    //Testing invalid adds
-		assertEquals("Feedback:  cannot be added!", LogicController.process("add        ", LogicController.getDisplayList()));
+		assertEquals("Feedback:  cannot be added!",
+					(LogicController.process("add        ", LogicController.getDisplayList())));
 	    logger.info("Adding blank task not acceptable throws the correct error feedback");
 	    
 	    Task sampleTaskTwo = new Task();
@@ -134,47 +203,47 @@ public class LogicControllerTest {
 		logger.info("Start of comparator tests");
 
 		//t1 has end no start; t2 has end no start.
-		LogicController.process("add 2103 Tutorial homework by today", LogicController.getDisplayList());
-		LogicController.process("add 2334 revision by next Sunday", LogicController.getDisplayList());
+		LogicController.process("add 2103 Tutorial homework due today", LogicController.getDisplayList());
+		LogicController.process("add 2334 revision by next Sunday 4pm", LogicController.getDisplayList());
 		
 		//t1 has end no start; t2 has start no end.
-		LogicController.process("add 3230 Homework by Friday", LogicController.getDisplayList());
-		LogicController.process("add 2101 Presentation on Friday", LogicController.getDisplayList());
+		LogicController.process("add 3230 Homework due Friday", LogicController.getDisplayList());
+		LogicController.process("add 2101 Presentation at Friday 4pm", LogicController.getDisplayList());
 		
 		//t1 has end no start, t2 has neither.
-		LogicController.process("add Finish revision by next Sunday", LogicController.getDisplayList());
+		LogicController.process("add Finish revision by Sunday 4pm", LogicController.getDisplayList());
 		LogicController.process("add Learn make-up", LogicController.getDisplayList());
 		
 		//t1 has end no start, t2 has start and end.
-		LogicController.process("add Stock up on drinks by 08/11", LogicController.getDisplayList());
+		LogicController.process("add Stock up on drinks due 08/11", LogicController.getDisplayList());
 		LogicController.process("add Conduct online course from Friday to Sunday", LogicController.getDisplayList());
 
 		//t1 has start no end, t2 has end no start.
-		LogicController.process("add Dinner with parents on 08/11", LogicController.getDisplayList());
-		LogicController.process("add 2103 Website by next Sunday", LogicController.getDisplayList());
+		LogicController.process("add Dinner with parents at 08/11 4pm", LogicController.getDisplayList());
+		LogicController.process("add 2103 Website due next Sunday", LogicController.getDisplayList());
 		
 		//t1 has start no end, t2 has start no end.
-		LogicController.process("add Watch documentary about anime at 07/11", LogicController.getDisplayList());
-		LogicController.process("add Facial appointment on Sunday", LogicController.getDisplayList());
+		LogicController.process("add Watch documentary about anime at 07/11 4pm", LogicController.getDisplayList());
+		LogicController.process("add Facial appointment at Sunday 4pm", LogicController.getDisplayList());
 
 		//t1 has start no end, t2 has end and start.
-		LogicController.process("add Cousin's Wedding on next Sunday", LogicController.getDisplayList());
+		LogicController.process("add Cousin's Wedding at next Sunday 4pm", LogicController.getDisplayList());
 		LogicController.process("add Finals from 24/11 to 03/12", LogicController.getDisplayList());
 
 		//t1 has start no end, t2 has neither.
-		LogicController.process("add Shop for clothes on 20/11", LogicController.getDisplayList());
+		LogicController.process("add Shop for clothes at 20/11 4pm", LogicController.getDisplayList());
 		LogicController.process("add Learning Japanese", LogicController.getDisplayList());
 
 		//t1 has both, t2 has start no end.
 		LogicController.process("add 2103 Project Finalisation from Wednesday to Sunday", LogicController.getDisplayList());
-		LogicController.process("add 3235 Steps preparation at Thursday", LogicController.getDisplayList());
+		LogicController.process("add 3235 Steps preparation at Thursday 4pm", LogicController.getDisplayList());
 		
 		//t1 has both, t2 has end no start.
 		LogicController.process("add 2334 tutorials from Thursday to Saturday", LogicController.getDisplayList());
-		LogicController.process("add Do supermarket shopping by Monday", LogicController.getDisplayList());
+		LogicController.process("add Do supermarket shopping by Sunday 4pm", LogicController.getDisplayList());
 		
 		//t1 has both, t2 has neither.
-		LogicController.process("add 2103 Handout and Slides from Monday to Wednesday", LogicController.getDisplayList());
+		LogicController.process("add 2103 Handout and Slides in 4 days", LogicController.getDisplayList());
 		LogicController.process("add Prepare for cosplay", LogicController.getDisplayList());
 		
 		//t1, t2 have both.
@@ -183,11 +252,11 @@ public class LogicControllerTest {
 		
 		//t1 has neither, t2 has end no start.
 		LogicController.process("add Exercise", LogicController.getDisplayList());
-		LogicController.process("add Complete 2334 webcast in 4 days", LogicController.getDisplayList());
+		LogicController.process("add Complete 2334 webcast by Sunday 4pm", LogicController.getDisplayList());
 				
 		//t1 has neither, t2 has start no end.
 		LogicController.process("add Reorganise nendoroids", LogicController.getDisplayList());
-		LogicController.process("add 2101 Presentation on Saturday", LogicController.getDisplayList());
+		LogicController.process("add 2101 Presentation at Saturday 9am", LogicController.getDisplayList());
 		
 		//t1 has neither, t2 has both.
 		LogicController.process("add Enjoy Life", LogicController.getDisplayList());
@@ -206,5 +275,3 @@ public class LogicControllerTest {
 	}
 	
 }
-
-
