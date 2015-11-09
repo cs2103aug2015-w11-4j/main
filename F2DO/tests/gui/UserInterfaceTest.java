9dac9fbc5591fbb09233c61a7509be4a30e58a82
Sufyan <yan91work@gmail.com>
2015-11-09 19:05:13 +0800

    Edited TestFX

--- F2DO/tests/gui/UserInterfaceTest.java
+++ F2DO/tests/gui/UserInterfaceTest.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.loadui.testfx.GuiTest;
 import org.loadui.testfx.utils.FXTestUtils;
@@ -14,9 +15,25 @@ import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 //@@author A0112882H-reused
-public class UserInterfaceTest extends GuiTest{
+public class UserInterfaceTest {
 	private static final SettableFuture<Stage> stageFuture = SettableFuture.create();
+	private static GuiTest controller;
+	
+	 @BeforeClass
+	    public static void setUpClass() {
+	            FXTestUtils.launchApp(UserInterface.class);
 
+	            controller = new GuiTest() {
+	                @Override
+	                protected Parent getRootNode() {
+	                    return stage.getScene().getRoot();
+	                }
+	            };
+	            System.out.println("GUI TEST START");
+
+	        }
+	
+	/*
 	@Override
 	protected Parent getRootNode() {
 		return stage.getScene().getRoot();
@@ -28,14 +45,39 @@ public class UserInterfaceTest extends GuiTest{
 		stage = targetWindow(stageFuture.get(25, TimeUnit.SECONDS));
 		FXTestUtils.bringToFront(stage);
 	}
-	
+	*/
 	@Test
 	public void test() throws Exception {
-		TextField textField = (TextField)GuiTest.find("#UITextField");
-		click(textField).type("add task").push(KeyCode.ENTER);
+		TextField textField = (TextField)GuiTest.find("_field");
+		System.out.println("TESTING GUI");
+		controller.click(textField).type("add task").push(KeyCode.ENTER);
+		
 	}
+	@Test
+	public void test2() throws Exception {
+		TextField textField = (TextField)GuiTest.find("_field");
+		System.out.println("TESTING GUI");
+		controller.click(textField).type("add task").push(KeyCode.ENTER);
+		
+	}
+	@Test
+	public void test3() throws Exception {
+		TextField textField = (TextField)GuiTest.find("_field");
+		System.out.println("TESTING GUI");
+		controller.click(textField).type("add task").push(KeyCode.ENTER);
+		
+	}
+	@Test
+	public void test4() throws Exception {
+		TextField textField = (TextField)GuiTest.find("_field");
+		System.out.println("TESTING GUI");
+		controller.click(textField).type("add task").push(KeyCode.ENTER);
+		
+	}
+	
 
-	/*
+
+/*	
 	@Test
 	public void testTextArea() {
 		TableView<S> table = (TableView)find("floatingTable");
